aa89a6c77a89d8eb1049518fe0576ec5af4f3fc5
Jovi Nel Parilla <jnp.parilla@gmail.com>
2022-05-02 14:57:30 +0800

    Initial commit

---
+++ VersionSQL/EC2AMAZ-6IN15V2/PromotionMoney_DB/dbo/Table/users.sql
@@ -0,0 +1,8 @@
+﻿/****** Object:  Table [dbo].[users]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[users](
+	[id] [bigint] NULL,
+	[name] [nchar](10) NULL
+) ON [PRIMARY]
